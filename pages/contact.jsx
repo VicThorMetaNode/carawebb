ea2707239e07239b54f9e9552e67e392201b5a39
unknown <git@github.com:VicThorMetaNode>
2022-09-26 18:31:33 +0200

    fixing contact form one line

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 // import Image from "next/image";
 
 import { Box, Text, Container, Stack, Icon, Flex } from "@chakra-ui/react";
@@ -19,33 +20,30 @@ const contact = () => {
         </nav>
         <Box className="contact-page-container" mt={10}>
           <Stack justifyContent="center" alignItems="center" direction="column">
-            <Container>
-              <Stack
-                justifyContent="center"
-                alignItems="center"
-                direction="column"
-              >
-                <Box className="contact-title-box">
-                  <Text className="contact-title">Contact</Text>
-                </Box>
+            <Stack
+              justifyContent="center"
+              alignItems="center"
+              direction="column"
+            >
+              <Box className="contact-title-box">
+                <Text className="contact-title">Contact</Text>
+              </Box>
 
-                <Box className="contact-subtitle-box">
-                  <Flex>
-                    <Text className="contact-subtitle" width={"13rem"}>
-                      Let's chat about your future memories
-                    </Text>
-                    <Box mt={"1.9rem"} ml={"-15px"}>
-                      <Icon
-                        as={BsSuitHeartFill}
-                        boxSize="1rem"
-                        color="#000000"
-                      />
-                    </Box>
-                  </Flex>
-                </Box>
-              </Stack>
-            </Container>
-
+              <Box className="contact-subtitle-box">
+                <Flex>
+                  <Text className="contact-subtitle" width={"23rem"}>
+                    Let's chat about your future memories
+                  </Text>
+                  <Box ml={"-40px"}>
+                    <Icon
+                      as={BsSuitHeartFill}
+                      boxSize="0.7rem"
+                      color="#000000"
+                    />
+                  </Box>
+                </Flex>
+              </Box>
+            </Stack>
             <Box>
               <Form />
             </Box>
