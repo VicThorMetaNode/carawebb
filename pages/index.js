29e190138504e22f222de110fd18f3bfc641d638
unknown <git@github.com:VicThorMetaNode>
2022-10-18 17:30:17 +0200

    test no scroll landingpage

--- pages/index.js
+++ pages/index.js
@@ -15,50 +15,52 @@ export default function Home() {
   return (
     <>
       <section className="landing-page">
-        <nav>
-          <Navbar />
-        </nav>
-        <Box mr={[3, 5, 20]} className="landing-page-section-title">
-          <Text fontSize="6xl" className="landing-page-title">
-            DEAR <br /> ONE
-          </Text>
-        </Box>
-        <Box mr={[3, 5, 20]} className="landing-page-section-subtitle">
-          <Text fontSize="2xl" className="landing-page-subtitle">
-            Let's build
-            <br />
-            memories together
-          </Text>
-        </Box>
-        <Box>
-          <Box mr={[3, 5, 20]} className="landing-page-btn">
-            <Link href="/contact">
-              <button className="btn-contact">CONTACT</button>
-            </Link>
+        <Box className="landing-page-box">
+          <nav>
+            <Navbar />
+          </nav>
+          <Box mr={[3, 5, 20]} className="landing-page-section-title">
+            <Text fontSize="6xl" className="landing-page-title">
+              DEAR <br /> ONE
+            </Text>
           </Box>
-        </Box>
+          <Box mr={[3, 5, 20]} className="landing-page-section-subtitle">
+            <Text fontSize="2xl" className="landing-page-subtitle">
+              Let's build
+              <br />
+              memories together
+            </Text>
+          </Box>
+          <Box>
+            <Box mr={[3, 5, 20]} className="landing-page-btn">
+              <Link href="/contact">
+                <button className="btn-contact">CONTACT</button>
+              </Link>
+            </Box>
+          </Box>
 
-        {/* <Flex justifyContent="right" alignItems="flex-end"> */}
-        <Box
-          className="landing-page-footer-logo"
-          width="90px"
-          height="100px"
-          position={"absolute"}
-          right={["0rem", "0rem", "3rem"]}
-        >
-          {/* position={"absolute"}
+          {/* <Flex justifyContent="right" alignItems="flex-end"> */}
+          <Box
+            className="landing-page-footer-logo"
+            width="90px"
+            height="100px"
+            position={"absolute"}
+            right={["0rem", "0rem", "3rem"]}
+          >
+            {/* position={"absolute"}
             right={[-2, -2, 7]}
             bottom={"0"} */}
-          <Image src={logoFooter} objectFit="cover" alt="Logo Picture" />
-        </Box>
-        {/* </Flex> */}
-        {/* ANCIEN FOOTER */}
-
-        <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
-          <Box className="landing-page-footer-section">
-            <Footer />
+            <Image src={logoFooter} objectFit="cover" alt="Logo Picture" />
           </Box>
-        </Flex>
+          {/* </Flex> */}
+          {/* ANCIEN FOOTER */}
+
+          <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+            <Box className="landing-page-footer-section">
+              <Footer />
+            </Box>
+          </Flex>
+        </Box>
       </section>
     </>
   );
