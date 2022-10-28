675d72e9dbec5c2d1290a4d0e70742a62abab134
unknown <git@github.com:VicThorMetaNode>
2022-10-28 13:47:51 +0200

    google analytics adding

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+//IMPORT GOOGLE ANALYTICS SETUP
+import * as ga from '../lib/google-analytics'
+
+//IMPORT SASS /design
 import "../scss/styles.scss"   
 
+//IMPORT CHAKRAUI LIBRARY
 import { ChakraProvider } from '@chakra-ui/react'
 
-
+//IMPORT INTERNAL LAYOUT SETUP
 import Layout from '../components/Layout'
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
   <ChakraProvider>
   <Layout>
