9321f8a8e29cecfb0457055dd1d1db74623835b7
unknown <git@github.com:VicThorMetaNode>
2022-10-30 14:48:22 +0100

    test full screen landing page

--- pages/landing_test.jsx
+++ pages/landing_test.jsx
@@ -1,7 +1,7 @@
 // ------------------ HOME PAGE -------------------
 import React from "react";
 
-import { Box, Text, Flex, Stack } from "@chakra-ui/react";
+import { Box, Text, Flex, Stack, Container } from "@chakra-ui/react";
 import Link from "next/link";
 
 //------ IMPORT COMPONENTS
@@ -11,33 +11,35 @@ import Footer from "../components/Footer_test";
 const landing_test = () => {
   return (
     <>
-      <Box>
-        <nav>
-          <Navbar />
-        </nav>
-      </Box>
+      <Container className="landing-page-test">
+        <Box>
+          <nav>
+            <Navbar />
+          </nav>
+        </Box>
 
-      <Box mr={[3, 5, 20]} mt="10rem">
-        <Text className="landing-page-title" fontSize="6xl">
-          DEAR <br /> ONE
-        </Text>
-      </Box>
-      <Box mr={[3, 5, 20]} mt="1.3rem">
-        <Text fontSize="2xl" className="landing-page-subtitle">
-          Let's build
-          <br />
-          memories together
-        </Text>
-      </Box>
-      <Box mr={[3, 5, 20]} className="landing-page-btn">
-        <Link href="/contact">
-          <button className="btn-contact">CONTACT</button>
-        </Link>
-      </Box>
+        <Box mr={[3, 5, 20]} mt="10rem">
+          <Text className="landing-page-title" fontSize="6xl">
+            DEAR <br /> ONE
+          </Text>
+        </Box>
+        <Box mr={[3, 5, 20]} mt="1.3rem">
+          <Text fontSize="2xl" className="landing-page-subtitle">
+            Let's build
+            <br />
+            memories together
+          </Text>
+        </Box>
+        <Box mr={[3, 5, 20]} className="landing-page-btn">
+          <Link href="/contact">
+            <button className="btn-contact">CONTACT</button>
+          </Link>
+        </Box>
 
-      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
-        <Footer />
-      </Flex>
+        <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+          <Footer />
+        </Flex>
+      </Container>
     </>
   );
 };
