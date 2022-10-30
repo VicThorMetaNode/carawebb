059f4d41b53f768776ae424cdbffb15350f3f354
unknown <git@github.com:VicThorMetaNode>
2022-10-30 12:48:52 +0100

    new calendly link

--- pages/commercialProjects.jsx
+++ pages/commercialProjects.jsx
@@ -169,7 +169,7 @@ const otherProjects = () => {
                       </Text>
                     </Box>
                     <Box className="interior-project-bottom-button-box">
-                      <Link href="https://calendly.com/caradearone/30min">
+                      <Link href="https://calendly.com/caradearone/15min">
                         <button className=" btn-discovery-call">
                           discovery call
                         </button>
