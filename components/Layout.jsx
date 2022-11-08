880057a0bbd2bbd097b7000e34a21acd6a8f6457
unknown <git@github.com:VicThorMetaNode>
2022-11-08 13:56:56 +0100

    test metatags + google analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,8 +8,50 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title className="head-title">cara.dearone</title>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>cara.DEARONE</title>
+        <meta name="title" content="cara.DEARONE" />
+        <meta
+          name="description"
+          content="      We design the interior to bring new perspectives and an
+                aesthetic look."
+        />
+        {/* <!-- FACICON -->  */}
         <link rel="shortcut icon" href="/favicon.ico" />
+
+        {/* <!-- SEARCH CONSOLE Meta Tags -->  */}
+        <meta
+          name="google-site-verification"
+          content="`${process.env.NEXT_PUBLIC_GOOGLE_SEARCH}`"
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="cara.DEARONE" />
+        <meta
+          property="og:description"
+          content="      We design the interior to bring new perspectives and an
+                aesthetic look."
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="cara.DEARONE" />
+        <meta
+          property="twitter:description"
+          content="      We design the interior to bring new perspectives and an
+                aesthetic look."
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
       </Head>
       <main>{children}</main>
       {/* <Footer /> */}
--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,63 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+// import { useEffect } from 'react'
+// import { useRouter } from 'next/router'
 
 //IMPORT GOOGLE ANALYTICS SETUP
-import * as ga from '../lib/google-analytics'
+// import * as ga from '../lib/google-analytics'
+import Script from "next/script";
 
 //IMPORT SASS /design
-import "../scss/styles.scss"   
+import "../scss/styles.scss";
 
 //IMPORT CHAKRAUI LIBRARY
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
 //IMPORT INTERNAL LAYOUT SETUP
-import Layout from '../components/Layout'
-
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+  // const router = useRouter()
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     ga.pageview(url)
+  //   }
+  //   //When the component is mounted, subscribe to router changes
+  //   //and log those page views
+  //   router.events.on('routeChangeComplete', handleRouteChange)
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+  //   // If the component is unmounted, unsubscribe
+  //   // from the event with the `off` method
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
   return (
-  <ChakraProvider>
-  <Layout>
-  <Component {...pageProps} />
-  </Layout>
-  </ChakraProvider>
-  )
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
